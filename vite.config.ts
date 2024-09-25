1017857cb9876898b2c74d0374c3fba21c6886a5
prayush21 <prayushdave@gmail.com>
2024-09-26 00:25:17 +0530

    Changes to fix dist folder

--- vite.config.ts
+++ vite.config.ts
@@ -4,11 +4,8 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      input: {
-        "service-worker": "src/service-worker.ts", // Add this
-      },
-    },
-  },
+  // input: {
+  // index: "src/index.html",
+  // "service-worker": "src/service-worker.ts", // Add this
+  // },
 });
